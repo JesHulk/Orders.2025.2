a7cbef8b52ca66ffd1ac7b7863ced20e68c1a02d
JesHulk <jeshulk07@gmail.com>
2025-08-31 08:42:02 +0200

    Add swagger

--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -3,17 +3,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+// Agregar Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 // Inyección de la conexión a la base de datos
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("LocalConnection")));
-
-
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    
+    // Habilitar Swagger UI
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Orders API V1");
+        c.RoutePrefix = string.Empty; // Esto hace que Swagger se muestre en la raíz (/)
+    });
 }
 
 app.UseHttpsRedirection();
