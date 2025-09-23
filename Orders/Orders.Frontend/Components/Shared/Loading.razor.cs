09874112aa7dc56a38c1a4163843577e1d58a656
JesHulk <jeshulk07@gmail.com>
2025-09-23 07:37:01 +0200

    Basic countries index

---
+++ Orders/Orders.Frontend/Components/Shared/Loading.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace Orders.Frontend.Components.Shared;
+
+public partial class Loading
+{
+    [Parameter] public string? Label { get; set; }
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        if (string.IsNullOrEmpty(Label))
+        {
+            Label = "Cargando, por favor, espere...";
+        }
+    }
+}
